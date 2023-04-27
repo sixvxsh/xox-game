8d9031a39d282ff959668250d150f21756775f2f
sixvxsh <sixvxsh.j@gmail.com>
2023-04-27 18:16:51 +0330

    init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Square({ value }) {
+    return <button className="square">{value}</button>;
+  }
+  
+  export default function Board() {
+    return (
+      <>
+        <div className="board-row">
+          <Square value="1" />
+          <Square value="2" />
+          <Square value="3" />
+        </div>
+        <div className="board-row">
+          <Square value="4" />
+          <Square value="5" />
+          <Square value="6" />
+        </div>
+        <div className="board-row">
+          <Square value="7" />
+          <Square value="8" />
+          <Square value="9" />
+        </div>
+      </>
+    );
+  }
+  
